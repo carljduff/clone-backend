48cfa326e3938474f1d87e4c68b585cf1f666f57
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-26 13:50:23 +0000

    created userauth

--- clones/migrations/0009_alter_event_img.py
+++ clones/migrations/0009_alter_event_img.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clones', '0008_exampleuser'),
+        ('clones', '0007_alter_event_status'),
     ]
 
     operations = [
--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
 
-AUTH_USER_MODEL = 'clones.CustomUser'
+AUTH_USER_MODEL = "clones.CustomUser"
+
 # Application definition
 
 INSTALLED_APPS = [
